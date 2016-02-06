71ccf9d16aed6c783c534be2e3d82db8e4f0d9a0
mgoelzer <mike@goelzer.com>
2016-02-05 20:27:55 -0800

    Added reboot-all.sh
    Signed-off-by: mgoelzer <mike@goelzer.com>

---
+++ Vagrant/reboot-all.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+ssh root@192.168.33.11 reboot now
+ssh root@192.168.33.20 reboot now
+ssh root@192.168.33.12 reboot now
+ssh root@192.168.33.200 reboot now
+ssh root@192.168.33.250 reboot now
+
